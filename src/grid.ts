e5862653ea9f7b84c4ef77b4897db334bdfb721c
MarkyMan4 <markus.hutnik@gmail.com>
2023-01-22 13:24:59 -0600

    implemented rules, added controls for drawing and running the rules

--- src/grid.ts
+++ src/grid.ts
@@ -1,14 +1,21 @@
 export default class Grid {
     private _cellsAcross: number;
     private _cellsDown: number;
-    private _cells: boolean[][]; // represents living/dead cells
+    public cells: boolean[][]; // represents living/dead cells
 
     constructor(cellsAcross: number, cellsDown: number) {
         this._cellsAcross = cellsAcross;
         this._cellsDown = cellsDown;
-        this._cells = [];
+        this.cells = [];
+        
+        // initialize the grid to all dead cells
+        this.resetCells();
+    }
 
-        // initialize the grid with dead cells
+    // set all cells to dead
+    public resetCells() {
+        this.cells = [];
+
         for(let i = 0; i < this._cellsDown; i++) {
             let cellRow = [];
 
@@ -16,11 +23,91 @@ export default class Grid {
                 cellRow.push(false);
             }
 
-            this._cells.push(cellRow);
+            this.cells.push(cellRow);
         }
     }
 
-    public get cells() {
-        return this._cells;
+    // recreate grid with random cells starting out alive
+    public randomize() {
+        this.cells = [];
+
+        for(let i = 0; i < this._cellsDown; i++) {
+            let cellRow = [];
+
+            for(let j = 0; j < this._cellsAcross; j++) {
+                cellRow.push(Math.random() >= 0.7);
+            }
+
+            this.cells.push(cellRow);
+        }
+    }
+
+    /*
+    If a living cell has two or three living neighbors, it survives
+    If a dead cell has three live neighbors, it becomes alive
+    All other cells die or stay dead
+    */
+    public evalRules() {
+        let newCells: boolean[][] = [];
+
+        for(let i = 0; i < this.cells.length; i++) {
+            let cellRow: boolean[] = [];
+
+            for(let j = 0; j < this.cells[i].length; j++) {
+                let livingNeighbors = this.getNumLivingNeighbors(j, i);
+                cellRow.push(this.getNewCellValue(this.cells[i][j], livingNeighbors));
+            }
+
+            newCells.push(cellRow);
+        }
+
+        this.cells = newCells;
+    }
+
+    /*
+    each cell has 8 neighboring cells
+    if the given indices are on the edge of the grid, the cells off the grid are considered dead
+    */
+    private getNumLivingNeighbors(x: number, y: number): number {
+        let count = 0;
+
+        if(y > 0 && x > 0 && this.cells[y - 1][x - 1]) { // above left
+            count++;
+        }
+        if(y > 0 && this.cells[y - 1][x]) { // above
+            count++;
+        }
+        if(y > 0 && x < this.cells[y].length - 1 && this.cells[y - 1][x + 1]) { // above right
+            count++;
+        }
+        if(x < this.cells[y].length - 1 && this.cells[y][x + 1]) { // right
+            count++;
+        }
+        if(y < this.cells.length - 1 && x < this.cells[y].length - 1 && this.cells[y + 1][x + 1]) { // down right
+            count++;
+        }
+        if(y < this.cells.length - 1 && this.cells[y + 1][x]) { // down
+            count++;
+        }
+        if(y < this.cells.length - 1 && x > 0 && this.cells[y + 1][x - 1]) { // down left
+            count++;
+        }
+        if(x > 0 && this.cells[y][x - 1]) { // left
+            count++;
+        }
+
+        return count;
+    }
+
+    // can probably write this logic more elegantly, this works for now though
+    private getNewCellValue(currentValue: boolean, numLivingNeighbors: number): boolean {
+        if(currentValue && (numLivingNeighbors === 2 || numLivingNeighbors === 3)) {
+            return true;
+        }
+        else if (!currentValue && numLivingNeighbors === 3) {
+            return true;
+        }
+
+        return false;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import Grid from './grid'
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+const runBtn = document.getElementById('run-btn') as HTMLButtonElement;
+const clearBtn = document.getElementById('clear-btn') as HTMLButtonElement;
+const randomizeBtn = document.getElementById('rand-btn') as HTMLButtonElement;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -11,6 +14,67 @@ const cellSize = canvas.width / cellsAcross;
 const cellsDown = Math.floor(canvas.height / cellSize);
 
 let grid = new Grid(cellsAcross, cellsDown);
+let isRunning = false;
+let isMouseDown = false;
+
+// controls for how fast animation runs
+let timestep = 75;
+// let currentTime = new Date();
+let lastUpdateTime = new Date();
+
+// button event listeners
+runBtn.addEventListener('click', (_) => {
+    isRunning = !isRunning;
+    lastUpdateTime = new Date();
+
+    runBtn.innerHTML = isRunning ? 'pause' : 'run';
+});
+
+clearBtn.addEventListener('click', (_) => {
+    grid.resetCells();
+});
+
+randomizeBtn.addEventListener('click', (_) => {
+    grid.randomize();
+});
+
+// gets the mouse position relative to canvas, not relative to screen
+const getMousePos = (ev: MouseEvent) => {
+    let boundingRect = canvas.getBoundingClientRect();
+    let scaleX = canvas.width / boundingRect.width;
+    let scaleY = canvas.height / boundingRect.height;
+
+    return {
+        x: (ev.clientX - boundingRect.left) * scaleX, 
+        y: (ev.clientY - boundingRect.top) * scaleY
+    };
+}
+
+document.addEventListener('mousedown', (ev: MouseEvent) => {
+    isMouseDown = true;
+
+    const {x: mouseX, y: mouseY} = getMousePos(ev);
+    
+    // find the cell that was clicked and flip isAlive
+    let x = Math.ceil((mouseX - cellSize) / cellSize);
+    let y = Math.ceil((mouseY - cellSize) / cellSize);
+
+    grid.cells[y][x] = !grid.cells[y][x];
+});
+
+document.addEventListener('mouseup', (_) => { isMouseDown = false; })
+
+document.addEventListener('mousemove', (ev: MouseEvent) => {
+    if(isMouseDown) {
+        const {x: mouseX, y: mouseY} = getMousePos(ev);
+    
+        // find the cell that was clicked and flip isAlive
+        let x = Math.ceil((mouseX - cellSize) / cellSize);
+        let y = Math.ceil((mouseY - cellSize) / cellSize);
+
+        grid.cells[y][x] = true;
+    }
+});
 
 const drawGrid = () => {
     for(let i = 0; i < grid.cells.length; i++) {
@@ -18,11 +82,26 @@ const drawGrid = () => {
             ctx.beginPath();
             ctx.rect(j * cellSize, i * cellSize, cellSize, cellSize);
             ctx.fillStyle = grid.cells[i][j] ? 'white' : 'black';
-            ctx.strokeStyle = 'white';
+            ctx.strokeStyle = grid.cells[i][j] ? 'black' : 'white';
             ctx.fill();
             ctx.stroke();
         }
     }
 }
 
-drawGrid();
+const animate = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawGrid();
+
+    if(isRunning) {
+        let currentTime = new Date();
+        if(currentTime.getTime() - lastUpdateTime.getTime() >= timestep) {
+            grid.evalRules();
+            lastUpdateTime = currentTime;
+        }
+    }
+
+    requestAnimationFrame(animate);
+}
+
+animate();
